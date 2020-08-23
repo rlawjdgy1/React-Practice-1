ce768dffdd8e0620c216e22fe0f9184c3d00f0be
Junghyo <junghyo@Junghyoui-MacBookPro.local>
2020-08-23 20:37:36 +0900

    react practice

---
+++ src/Counter.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+class Counter extends Component {
+    state = {
+        number: 0,
+        error: false
+    }
+
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount(){
+        console.log('componentWillMount (deprecated)');
+    }
+    
+    componentDidMount(){
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps,nextState){
+        console.log('shouldComponentUpdate');
+        if (nextState.number % 5 === 0) return false;
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState){
+        console.log('componentWillUpdate');
+    }
+
+    handleIncrease = () => {
+        const { number } = this.state;
+        this.setState({
+            number: number + 1
+        });
+    }
+
+    handleDecrease = () => {
+        this.setState(
+            ({ number }) => ({
+                number: number - 1
+            })
+        );
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+    }
+
+    render() {
+        if (this.state.error) return (<h1>에러발생!</h1>);
+
+        return (
+            <div>
+                <h1>카운터</h1>
+                <div>값: {this.state.number}</div>
+                <button onClick={this.handleIncrease}>+</button> 
+                <button onClick={this.handleDecrease}>-</button>
+            </div>
+        );
+    }
+}
+
+export default Counter;
